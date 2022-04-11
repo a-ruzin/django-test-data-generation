a9c0188dd1373bbf3fb54a5582234d8db992001b
Alexey Ruzin <ruzin@kokoc.com>
2022-04-11 13:18:33 +0300

    example of uniq constraint created as a function

--- store/models.py
+++ store/models.py
@@ -1,10 +1,16 @@
 from django.db import models
+from django.db.models.functions import Lower
 
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    class Meta:
+        constraints = (
+            models.UniqueConstraint(Lower('name').desc(), name='unique_name'),
+        )
 
     def __str__(self):
         return f'{self.name} - {self.price}'
